05edc2d28e91a27dab085896e5d6aaf846d678b4
HapiFive <1143910315@qq.com>
2020-07-05 15:28:09 +0800

    新界面版本

--- windowsmethod.cpp
+++ windowsmethod.cpp
@@ -20,35 +20,35 @@ long windowsMethod::a() {
 }
 
 unsigned int windowsMethod::GetWuXiaProcess() {
-    QFile outFile("shanchu.txt");
-    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
-    QTextStream ts(&outFile);
-    HWND topWindow1 = GetTopWindow(0);
-    while(topWindow1 != 0) {
-        HWND parent1 = GetParent(topWindow1);
-        int windowTextLength1 = GetWindowTextLength(topWindow1);
-        LPTSTR a1 = new TCHAR[windowTextLength1 + 1];
-        GetWindowText(topWindow1, a1, windowTextLength1 + 1);
-        QString localFromUtf16 = QString::fromUtf16((char16_t *)a1);
-        RECT r;
-        r.top = 0;
-        r.left = 0;
-        r.bottom = 0;
-        r.right = 0;
-        GetWindowRect(topWindow1, &r);
-        ts << QString::number((int)topWindow1) << "|" << QString::number((int)parent1) << "|" << localFromUtf16 << "|" << QString::number(r.left) << "|" << QString::number(r.top) << "|" << QString::number(r.right) << "|" << QString::number(r.bottom) << "\n";
-        delete[] a1;
-        topWindow1 = GetWindow(topWindow1, GW_HWNDNEXT);
-    }
-    int i = 0;
+    //QFile outFile("shanchu.txt");
+    //outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    //QTextStream ts(&outFile);
+    //HWND topWindow1 = GetTopWindow(0);
+    //while(topWindow1 != 0) {
+    //    HWND parent1 = GetParent(topWindow1);
+    //    int windowTextLength1 = GetWindowTextLength(topWindow1);
+    //    LPTSTR a1 = new TCHAR[windowTextLength1 + 1];
+    //    GetWindowText(topWindow1, a1, windowTextLength1 + 1);
+    //    QString localFromUtf16 = QString::fromUtf16((char16_t *)a1);
+    //    RECT r;
+    //    r.top = 0;
+    //    r.left = 0;
+    //    r.bottom = 0;
+    //    r.right = 0;
+    //    GetWindowRect(topWindow1, &r);
+    //    ts << QString::number((int)topWindow1) << "|" << QString::number((int)parent1) << "|" << localFromUtf16 << "|" << QString::number(r.left) << "|" << QString::number(r.top) << "|" << QString::number(r.right) << "|" << QString::number(r.bottom) << "\n";
+    //     delete[] a1;
+    //     topWindow1 = GetWindow(topWindow1, GW_HWNDNEXT);
+    //}
+    //int i = 0;
     HWND topWindow = GetTopWindow(0);
     while(topWindow != 0) {
-        /*for (int i = 0; i < process.Length; i++) {
-            if (topWindow.Equals(process[i].MainWindowHandle)) {
-                return process[i];
-            }
-        }*/
-        i++;
+        //for (int i = 0; i < process.Length; i++) {
+        //    if (topWindow.Equals(process[i].MainWindowHandle)) {
+        //        return process[i];
+        //    }
+        //}
+        //i++;
         HWND parent = GetParent(topWindow);
         if(parent == 0) {
             int windowTextLength = GetWindowTextLength(topWindow);
@@ -57,8 +57,8 @@ unsigned int windowsMethod::GetWuXiaProcess() {
                 GetWindowText(topWindow, a, windowTextLength + 1);
                 QString localFromUtf16 = QString::fromUtf16((char16_t *)a);
                 if(localFromUtf16 == "天涯明月刀") {
-                    ts << QString("非超退出") << QString::number(i);
-                    ts.flush();
+                    //ts << QString("非超退出") << QString::number(i);
+                    //ts.flush();
                     delete[] a;
                     return (unsigned int)topWindow;
                 }
@@ -68,9 +68,9 @@ unsigned int windowsMethod::GetWuXiaProcess() {
         }
         topWindow = GetWindow(topWindow, GW_HWNDNEXT);
     }
-    ts << QString("超退出！");
-    ts.flush();
-    return 0;
+    //ts << QString("超退出！");
+    //ts.flush();
+    return -1;
 }
 
 
