5c8663921605c8b3c7697eeef814e1d2872889c0
HapiFive <1143910315@qq.com>
2022-02-26 11:13:57 +0800

    I18n化

--- QinDevilClient_zh_CN.ts
+++ QinDevilClient_zh_CN.ts
@@ -1,3 +1,172 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="QinDevilClient_zh_CN"></TS>
+<TS version="2.1" language="QinDevilClient_zh_CN">
+<context>
+    <name>Dialog</name>
+    <message>
+        <source>输入</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>请输入昵称：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LogDialog</name>
+    <message>
+        <source>查看日志</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>上一页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>当前页数：0   共0页</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>下一页</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>琴魔副本工具</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ping:9999ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>用户名：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>作者：琴魔一梦之队 Q群：1126326900</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>一号琴</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>输入数字</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>二号琴</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>三号琴</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>四号琴</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>F键补：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>自动分配</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>宫</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>商</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>角</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>徵</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>羽</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>剩余弹琴数：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;按ctrl+数字键盘12345，来对应琴魔头顶的宫商角徵羽&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>需要弹琴键：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>9 8 7 6 5 4 3 2 1 </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>三分损益</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>小怪吃球顺序</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>宫→徵→商→羽→角</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&#x3000;本身 吃一个 吃两个 吃三个 吃四个</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>显示日志</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>隐藏任务栏</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>隐藏补琴人</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>隐藏打怪提示</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>关闭</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Password</name>
+    <message>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>密码认证</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
